a7e73fe1ae563e1c89bf814415ac86cca664ad90
Nhan <nhan@Nhans-MacBook-Pro.local>
2023-06-02 08:15:31 +0700

    Add headlight_plugin

---
+++ PassengerWelcome.js
@@ -0,0 +1,122 @@
+import SignalWithMedia from "./reusable/SignalWithMedia.js"
+import SignalPills from "./reusable/SignalPills.js"
+
+const plugin = ({widgets, vehicle, simulator}) => {
+    widgets.register(
+        "LightDome",
+        SignalWithMedia("Vehicle.Cabin.Lights.IsDomeOn", {
+            [true]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/BatrayDomeLightOn720x360.mp4"
+            },
+            [false]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/BatrayDomeLightOff720x360.mp4"
+            },
+        }, 
+        vehicle)
+    )
+
+    widgets.register(
+        "SeatUpDown",
+        SignalWithMedia("Vehicle.Cabin.Seat.Row1.Pos1.Height", {
+            [0]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/SeatGoDown720x360.mp4"
+            },
+            [100]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/SeatGoUp720x360.mp4"
+            },
+        }, vehicle)
+    )
+    
+    widgets.register(
+        "LeftDoorOpen",
+        SignalWithMedia("Vehicle.Cabin.Door.Row1.Left.IsOpen", {
+            [true]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/DoorOpen720x360.mp4"
+            },
+            [false]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/DoorClose720x360.mp4"
+            },
+        }, vehicle)
+    )
+    
+    widgets.register(
+        "MirrorTilt",
+        SignalWithMedia("Vehicle.Body.Mirrors.Left.Tilt", {
+            [0]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/MirrorsOpening720x360.mp4"
+            },
+            [100]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/MirrorsClose720x360.mp4"
+            },
+        }, vehicle)
+    )
+
+    widgets.register(
+        "SeatEngaged",
+        SignalWithMedia("Vehicle.Cabin.Seat.Row1.Pos1.Switch.Seating.IsForwardEngaged", {
+            [true]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/SeatGoToFront720x360.mp4"
+            },
+            [false]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/SeatGoToRear720x360.mp4"
+            },
+        }, vehicle)
+    )
+    
+    widgets.register(
+        "Tilt",
+        SignalWithMedia("Vehicle.Cabin.Seat.Row1.Pos1.Tilt", {
+            [0]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/SeatRotationFront720x360.mp4"
+            },
+            [100]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/SeatRotationBack720x360.mp44"
+            },
+        }, vehicle)
+    )
+    
+    widgets.register(
+        "AmbientLight",
+        SignalWithMedia("Vehicle.Cabin.Lights.AmbientLight", {
+            [0]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/Ambience720x360.mp4"
+            },
+            [100]: {
+                type: "video",
+                url: "https://digitalauto-media-data.netlify.app/Ambience720x360.mp4"
+            },
+        }, vehicle)
+    )
+    
+    widgets.register(
+        "SignalPillsConsumer",
+        SignalPills(
+            [
+                {
+                    signal: "Vehicle.Cabin.Door.Row1.Left.IsOpen",
+                    icon: `fa-gauge`
+                },
+                {
+                    signal: "Vehicle.Cabin.Seat.Row1.Pos1.Height",
+                    icon: `fa-gauge`
+                }
+            ],
+            vehicle
+        )
+    )
+}
+
+export default plugin
--- README.md
+++ README.md
@@ -1,15 +1,2 @@
-### This repository is currently being used to host static projects created in the [Packetcode](https://youtube.com/packetcode) YouTube channel.
-
-
-**Projects Showcase** : <https://packetcode.github.io>
-
-**YouTube Channel** : <https://youtube.com/packetcode>
-
----
-
-**Official Webiste**: [Packetcode](https://packetcode.in)
-
-**Email** : <packetcodeofficial@gmail.com>
-
-**Telegram**: [Packetcode](https://t.me/packetcode)
-# nhan-page-002.github.io
+This repo give you some sample plugins
+Read through the code to understand how a plugin working, and then you can built one.
